b42bdebf02fe683665a6f9cb40c41dd929914c4d
mn15104 <mn15104@my.bristol.ac.uk>
2018-07-01 17:34:43 +0100

    linear regression finished

--- linear_regression.py
+++ linear_regression.py
@@ -2,10 +2,12 @@ import numpy as np
 import scipy.stats as st
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import matplotlib.mlab as mlab
 
 # y = w x + e
 
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 def linear_regression():
     figPlot = plt.figure(figsize=(10,10))
@@ -25,18 +27,24 @@ def linear_regression():
     x_samples = np.zeros(1000)
     w_samples = np.zeros(1000)
     y_samples = np.zeros(1000)
+    w_mu_samples = np.zeros(1000)
+    cov_samples = np.zeros(1000)
+    gaussian_plots = np.zeros(1000)
     for i in range(n):
        
     
         x_samples[i] = np.random.uniform(-10, 10)
         w_samples[i] = np.random.normal(mu, sigma)
         y_samples[i] = x_samples[i] * w_samples[i]
+        w_mu  = np.mean(w_samples[0:k])
+        cov = np.var(w_samples[0:k])
+
+        w_mu_samples[i] = np.mean(w_samples[0:k])
+        cov_samples[i] = cov
         k = k + 1
 
         ###### W Samples vs. Sample Number
         axGraph.clear()
-        w_mu  = np.mean(w_samples[0:k])
-        cov = np.var(w_samples[0:k])
         axGraph.plot(w_samples[0:k], 'x') 
 
         ###### Updated linear function y = w * x + e
@@ -44,22 +52,12 @@ def linear_regression():
         t2 = [-5 * w_mu, 5 * w_mu]
         axPlot.plot(t1, t2) 
 
-        # ######## Contour
-        # axContour.clear()
-        # N = 60
-        # X = np.linspace(-2.5, 5, N)
-        # Y = np.linspace(-2.5, 5, N)
-        # X, Y = np.meshgrid(X, Y)
-
-        # mu = np.array([x_mu, y_mu])
-        # Sigma = np.array(cov)
-
-        # pos = np.empty(X.shape + (2,))
-        # pos[:, :, 0] = X
-        # pos[:, :, 1] = Y
-        # Z = multivariate_gaussian(pos, mu, Sigma)
-
-        # cset = axContour.contour(X, Y, Z, zdir='z')
+        ###### Updated W Normal Distribution
+        axContour.clear()
+        
+        for j in range (k):
+            gaussian_plot = np.linspace(w_mu_samples[i] - 3*cov_samples[i], w_mu_samples[i] + 3*cov_samples[i], 100)
+            axContour.plot( gaussian_plot, mlab.normpdf(gaussian_plot, w_mu_samples[j], cov_samples[j]))
         
         plt.draw()
         plt.waitforbuttonpress()
